de9451247b152986fffe2ad3c82a6eaddb5bca2d
soruma <soruma.net@gmail.com>
2018-01-01 12:09:48 +0900

    add ramen images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +6,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.create(name: 'いちごパフェスパ', price: 860, image_url: '/uploads/product/image/ramen/D1000003.JPG')
+Product.create(name: 'かきラーメン', price: 1300, image_url: '/uploads/product/image/ramen/IMG_4212.JPG')
+Product.create(name: '薩摩', price: 1300, image_url: '/uploads/product/image/ramen/IMG_4214.JPG')
+Product.create(name: '大貫中華そば', price: 850, image_url: '/uploads/product/image/ramen/IMG_4884.JPG')
+Product.create(name: 'ビーフンスープ', price: 300, image_url: '/uploads/product/image/ramen/P1010446.JPG')
+Product.create(name: 'まぜ麺', price: 850, image_url: '/uploads/product/image/ramen/IMG_7341.JPG')
+Product.create(name: 'チーズラーメン', price: 1000, image_url: '/uploads/product/image/ramen/IMG_6778.JPG')
